326fc270d693f4e76e1cf13be6bda9c228c6326d
Taha Kiany <tahakiany9@gmail.com>
2022-02-10 12:33:32 +0330

    add javascript files to my project

---
+++ js/main.js
@@ -0,0 +1,68 @@
+$('#main_slider').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    },
+    autoplay: false,
+})
+$('#main2_slider').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    },
+    autoplay: false,
+})
+$('#multi_slider').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 3
+        }
+    },
+    autoplay: false,
+})
+$('#comments').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 5
+        }
+    },
+    autoplay: false,
+})
